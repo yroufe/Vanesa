a3ae8f45c5fbee0ee8654b7fac5cdca6c7efd6ed
Yael Roufe <yaelroufe@gmail.com>
2022-10-18 18:31:58 -0300

    tarea arrays

--- Tarea - Array y funciones de orden superior/Tarea/tarea.js
+++ Tarea - Array y funciones de orden superior/Tarea/tarea.js
@@ -13,11 +13,50 @@ const personas = [
     { nombre: 'Aaron', appellido: 'Garrett', fechaNacimiento: '1985-09-04', profesion: 'Desarrollador', salario: 39000 },
 ];
 
+console.log(personas);
+
 /*
     1 - Buscar a todas las personas que tengan como profesion 'Desarrollador'. El resultado debe ser un array.
     2 - Ordena el array de personas por el salario de menor a mayor.
     3 - Calcular el salario promedio
     4 - Mostrar un array nuevo que muestre a las personas mayores de 35 (usen el objeto Math y Date para lograrlo)
-
 */
 
+//1) Utilice con método filter para atraer todos los que tengan 'Desarrollador'
+
+let buscaDesarrollador = personas.filter(pers => pers.profesion == 'Desarrollador');
+console.log(buscaDesarrollador);
+
+//2) Utilice sort para ordenar de menor a mayor el salario de cada uno
+let ordenaSalario = personas.sort((a,b)=> {
+    if (a.salario > b.salario) {
+        return 1
+    }
+    if (a.salario < b.salario) {
+        return -1
+    }
+    return 0
+})
+console.log(ordenaSalario);
+//----------------------------------------------------------------
+
+// 3) Utilice reduce para calcular el salador promedio
+const promedioSalario = personas.reduce((acumulador, pers) => {
+    return (acumulador + pers.salario)/personas.length;
+}, 0)
+
+console.log(promedioSalario);
+//----------------------------------------------------------------
+
+// 4) Utilice map para crear una nueva lista y luego con Date para convertir string a numero.
+const listaNuevaCumple = [];
+personas.forEach(persona => {
+    let cumpleanos = new Date(persona.fechaNacimiento);
+    let hoy = new Date();
+    let edad = hoy.getFullYear() - cumpleanos.getFullYear();
+    
+    if(edad > 35){
+        listaNuevaCumple.push(persona)
+    }
+})
+console.log(listaNuevaCumple)
